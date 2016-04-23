feca8dacba9fc65149743c709155ef543514eaa3
vmelania <voiosmelania@gmail.com>
2016-04-23 13:26:46 +0300

    Added files via upload

---
+++ Csaladi.java
@@ -0,0 +1,27 @@
+
+public class Csaladi extends Auto {
+
+	protected boolean gyerekules;
+	protected boolean csomagtarto;
+	
+	public Csaladi(int sebesseg, String valto, int fogyasztas, int ar, String allapot, int ferohelyek, int veszely, boolean gyerekules, boolean csomagtarto) {
+		super(sebesseg, valto, fogyasztas, ar, allapot, ferohelyek, veszely);
+		this.gyerekules =  gyerekules;
+		this.csomagtarto = csomagtarto;
+		
+	}
+	public void kiir(){
+		System.out.println("Sebbeseg:"+sebesseg);
+		System.out.println("Valto:"+valto);
+		System.out.println("Fogyasztas:"+fogyasztas);
+		System.out.println("Ar:"+ar);
+		System.out.println("Allapot:"+allapot);
+		System.out.println("Ferohelyek:"+ferohelyek);
+		System.out.println("Veszely szint:"+veszely);
+		System.out.println("Gyerekules:"+gyerekules);
+		System.out.println("Csomagtarto:"+csomagtarto);
+	}
+
+	
+
+}
